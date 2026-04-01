72854571058a6cc75295df0a7f93880aa7f384fb
AH4761 <AH4761@student.jamk.fi>
2026-04-01 20:28:22 +0300

    add

--- shell/shell-3/L03T01.sh
+++ shell/shell-3/L03T01.sh
@@ -12,17 +12,3 @@ echo "----- Tallennetut arvot -----"
 for i in "${!values[@]}"; do
     echo "$i: ${values[$i]}"   # Tulostetaan index: value
 done
-
-
-cd /c/Users/1/Documents/tt00cd76/shell/shell-3
-./L03T01.sh
-./L03T02.sh
-./L03T03.sh
-/c/Users/1/Documents
-
-
-./L03T04.sh
-/c/Users/1/Documents
-/c/Users/1/Desktop
-
-./L03T05.sh
